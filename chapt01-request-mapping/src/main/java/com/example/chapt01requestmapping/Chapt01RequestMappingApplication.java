42b0265f6d284a0eff66ba7a28f34b1aa14e2491
Jin-tonix <jinheemok815@gmail.com>
2024-06-13 17:31:33 +0900

    first commit

---
+++ chapt01-request-mapping/src/main/java/com/example/chapt01requestmapping/Chapt01RequestMappingApplication.java
@@ -0,0 +1,13 @@
+package com.example.chapt01requestmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapt01RequestMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapt01RequestMappingApplication.class, args);
+	}
+
+}
---
+++ chapt01-request-mapping/src/test/java/com/example/chapt01requestmapping/Chapt01RequestMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapt01requestmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapt01RequestMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

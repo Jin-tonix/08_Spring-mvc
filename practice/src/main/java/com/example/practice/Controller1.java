2f512aa2182d7097cc8cd64b29891769808269c6
Jin-tonix <jinheemok815@gmail.com>
2024-06-18 15:06:14 +0900

    123

--- practice/src/main/java/com/example/practice/Controller1.java
+++ practice/src/main/java/com/example/practice/Controller1.java
@@ -1,36 +1,44 @@
 package com.example.practice;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Controller1 {
 
-    // 루트 경로 또는 /index 경로로 GET 요청이 들어왔을 때 index1.jsp를 보여줌
-    @RequestMapping(value={"/", "/index"}, method = RequestMethod.GET)
+    // 루트 경로 또는 /index 경로로 GET 요청이 들어왔을 때 index1.html을 보여줌
+    @GetMapping(value={"/", "/index"})
     public String showIndexPage() {
         return "index1"; // view의 이름을 리턴
     }
 
     // /submitForm 경로로 POST 요청이 들어왔을 때 실행됨
-    @RequestMapping(value = "/submitForm", method = RequestMethod.POST)
+    @PostMapping("/submitForm")
     public ModelAndView handleSubmitForm(@RequestParam("inputData") String inputData) {
         System.out.println("전송된 데이터: " + inputData);
 
         // ModelAndView 객체 생성
         ModelAndView modelAndView = new ModelAndView();
 
-        // 뷰의 이름을 설정 (WEB-INF/views/index1-1.jsp로 리다이렉트)
-        modelAndView.setViewName("index1-1");
+        // 뷰의 이름을 설정 (index1-1.html로 리다이렉트)
+        modelAndView.setViewName("redirect:/index1-1");
 
         // 전송된 데이터를 모델에 추가
-        modelAndView.addObject("inputData", inputData);
+        modelAndView.addObject("userInput", inputData);
 
         // ModelAndView 객체 반환
         return modelAndView;
     }
 
+    // index1-1.html를 보여주기 위한 메서드
+    @GetMapping("/index1-1")
+    public String showIndex1_1Page(@RequestParam(value = "userInput", required = false) String userInput, Model model) {
+        // 모델에 데이터 추가
+        model.addAttribute("userInput", userInput);
+        return "index1-1"; // view의 이름을 리턴
+    }
 }
